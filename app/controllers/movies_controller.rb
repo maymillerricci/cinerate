6e099f36b119d859d99af33dd38d131856b2d172
May Miller-Ricci <maymillerricci@gmail.com>
2015-04-04 11:59:42 -0700

    add movie api info and get a list of movie info working as an example

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,8 @@
 class MoviesController < ApplicationController
   def index
-
+    url = "#{API_BASE_URL}/genre/12/movies?api_key=#{API_KEY}"
+    resource = RestClient::Resource.new(url)
+    raw_data = resource.get(:accept => :json)
+    @movies = JSON.parse(raw_data)["results"]
   end
 end
---
+++ config/initializers/movies_api.rb
@@ -0,0 +1,2 @@
+API_BASE_URL = "http://api.themoviedb.org/3"
+API_KEY = "4d5fd42298eee046ace1ea411c3cdb85"
