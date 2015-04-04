593d41956ef75dbd0174d6a5e914d698104262db
May Miller-Ricci <maymillerricci@gmail.com>
2015-04-04 16:13:14 -0700

    add validations to review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,6 @@
 class Review < ActiveRecord::Base
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+
+  validates_format_of :user_email, with: VALID_EMAIL_REGEX, message: "is not a valid email address"
+  validates_numericality_of :rating, greater_than_or_equal_to: 0, less_than_or_equal_to: 10
 end
