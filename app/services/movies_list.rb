f3c25b6bd67995006a14233cea62d72d76db7956
May Miller-Ricci <maymillerricci@gmail.com>
2015-04-04 13:28:10 -0700

    move api call code into method

--- app/services/movies_list.rb
+++ app/services/movies_list.rb
@@ -5,7 +5,6 @@ class MoviesList
 
     get_genres.each do |genre|
       movies_array = get_movies_of_a_genre(genre)
-      puts "API"
       movies_by_genre[genre["name"]] = movies_array
     end
 
@@ -17,14 +16,17 @@ class MoviesList
   # returns list of genres w/id & name
   def get_genres
     url = "#{API_BASE_URL}/genre/movie/list?api_key=#{API_KEY}"
-    raw_data = RestClient.get(url, :accept => :json)
-    JSON.parse(raw_data)["genres"]
+    api_call(url)["genres"]
   end
 
   def get_movies_of_a_genre(genre)
     url = "#{API_BASE_URL}/genre/#{genre["id"]}/movies?api_key=#{API_KEY}"
+    api_call(url)["results"]
+  end
+
+  def api_call(url)
     raw_data = RestClient.get(url, :accept => :json)
-    JSON.parse(raw_data)["results"]
+    JSON.parse(raw_data)
   end
 
 end
