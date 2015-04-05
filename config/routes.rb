86365d34ab2b778c0de039d72dda1d9874b0f233
May Miller-Ricci <maymillerricci@gmail.com>
2015-04-04 19:21:58 -0700

    add link to read all movie reviews

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root "movies#index"
+  get "/reviews", to: "reviews#index"
   resources :movies, only: [:index, :show] do
-    resources :reviews, only: [:index, :new, :create]
+    resources :reviews, only: [:new, :create]
   end
 end
