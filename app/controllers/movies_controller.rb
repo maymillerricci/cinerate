9ec51385fd6713ac288fa18f09dcb398e76fb972
May Miller-Ricci <maymillerricci@gmail.com>
2015-04-04 18:49:25 -0700

    start show page for movie with reviews

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,13 @@
 class MoviesController < ApplicationController
+
   def index
     @movies_by_genre = MoviesList.new.movies_by_genre
   end
+
+  def show
+    @title = params[:movie_title]
+    @reviews = Review.where(movie_id: params[:id])
+  end
+
+  
 end
